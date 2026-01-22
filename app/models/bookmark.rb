6afbab912bf16f8d77814d35eb01b717bd3adc58
Hisham <hisham.khashman@gmail.com>
2026-01-22 22:44:58 +0900

    changes

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
+  attr_accessor :movie_title, :movie_overview, :movie_poster_url, :movie_rating
+
   validates :comment, presence: true, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list_id }
 end
